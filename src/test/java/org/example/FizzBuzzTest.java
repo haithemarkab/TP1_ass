50e7e9b852e754bf6aa9c224234f9c53d991e38a
haithemarkab <haithemarkab8@gmail.com>
2024-03-28 10:13:31 +0100

    19

--- src/test/java/org/example/FizzBuzzTest.java
+++ src/test/java/org/example/FizzBuzzTest.java
@@ -4,48 +4,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-
-
-
-
-
 public class FizzBuzzTest {
     private FizzBuzz fizzBuzzTest;
-
-
-
     @BeforeEach
     void SetUp() {
         fizzBuzzTest = new FizzBuzz();
     }
     @Test
-    public void testMultiple3() {
+    public void testNegative() {
+        assertThrows(IllegalArgumentException.class, () -> fizzBuzzTest.fizzBuzz(-19));
+    }
+    @Test
+    public void testMultiple_3() {
         assertEquals("Fizz", fizzBuzzTest.fizzBuzz(6));
     }
-
-
-
-
-
-
     @Test
-    public void testMultiple3andMultiple5() {
+    public void testMultiple3andMultiple_5() {
         assertEquals("FizzBuzz", fizzBuzzTest.fizzBuzz(30));
     }
     @Test
-    public void testNotAnyMultiple() {
+    public void testNotAnyMultiple3_5() {
         assertEquals("13", fizzBuzzTest.fizzBuzz(13));
     }
 
     @Test
-    public void testMultiple5() {
+    public void testMultiple_5() {
         assertEquals("Buzz", fizzBuzzTest.fizzBuzz(25));
     }
 
 
-    @Test
-    public void testNegative() {
-        assertThrows(IllegalArgumentException.class, () -> fizzBuzzTest.fizzBuzz(-19));
-    }
+
 
 }
